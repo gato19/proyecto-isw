8ce49b6ecee3cc78a200fa714721672aa36e7bd8
Hector Riquelme <hectorariquelmec@gmail.com>
2016-06-05 18:29:53 -0300

    commit 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 2015 GG </div>
+                <div class="title">Laravel 2015 GG IZI </div>
             </div>
         </div>
     </body>
