5332f168e6f8e11864703d372779c7addf805eab
Ricardo Saez <ricsaez@alumnos.ubiobio.cl>
2016-06-05 19:20:24 -0300

    Cambio Welkome 2015

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Larave </div>
+                <div class="title">Laravel 2015 </div>
             </div>
         </div>
     </body>
