1bd99d604a660af3514f54ca9382ade4511f6cb4
Hector Riquelme <hectorariquelmec@gmail.com>
2016-06-05 15:13:59 -0300

    rtewytr

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Laravel asd</div>
             </div>
         </div>
     </body>
